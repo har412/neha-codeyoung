7cd722d7d6fb2d3e4317c2129686b0a4cb53e9df
Harkirat <hs3188248@gmail.com>
2023-03-11 21:27:48 +0530

    table using loops

---
+++ loops/js/loops.js
@@ -0,0 +1,21 @@
+function printTable(){
+// here we grab the number which user enters
+var number = document.getElementById('number').value;
+
+var table = document.getElementById('table');
+table.innerHTML=""; 
+
+for(var i = 1 ; i<=50;i++){
+
+    var row = table.insertRow();
+    var col1 = row.insertCell();
+    var col2 = row.insertCell();
+
+    var product = i * number;
+
+    col1.innerHTML= number + " x " + i + " = " ;
+    col2.innerHTML=product;
+
+}
+
+}
